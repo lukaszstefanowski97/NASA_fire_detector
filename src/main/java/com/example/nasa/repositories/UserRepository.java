d993c2a67bf4d5be1809175a84b0e99ca6632db4
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-10-19 10:28:29 +0200

    ADD user entity repository

---
+++ src/main/java/com/example/nasa/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.nasa.repositories;
+
+import com.example.nasa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
