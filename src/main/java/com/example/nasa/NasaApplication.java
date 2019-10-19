a8dcf70e2db8d35247af3adc06f2c567948533cd
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-10-19 10:15:29 +0200

    ADD project

---
+++ src/main/java/com/example/nasa/NasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nasa/NasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
