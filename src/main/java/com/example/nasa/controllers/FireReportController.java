388c90fa5bce6c559912eb33e38e44e325d731de
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-10-19 20:32:29 +0200

    fixes

--- src/main/java/com/example/nasa/controllers/FireReportController.java
+++ src/main/java/com/example/nasa/controllers/FireReportController.java
@@ -53,7 +53,7 @@ public class FireReportController {
                 fireReportDTO.getDescription(), fireReportDTO.getFireDepartamentDescription(),
                 fireReportDTO.getArePeople(), fireReportDTO.getIsWood(),
                 fireReportDTO.getIsBuilding(), fireReportDTO.getIsElectricity(),
-                fireReportDTO.getIsHazardousMaterial(), fireReportDTO.getPhoto()));
+                fireReportDTO.getIsHazardousMaterial(), fireReportDTO.getPhoto());
 
         fireReport.setReporterId(userDTO.getId());
         fireReportService.saveFireReport(fireReport);
@@ -70,11 +70,11 @@ public class FireReportController {
         FireReport fireReport = new FireReport(fireReportDTO.getReporterId(),
                 fireReportDTO.getX(), fireReportDTO.getY(),
                 fireReportDTO.getStartDate(), fireReportDTO.getFireReportApproveCounter(),
-                fireReportDTO.getFireTeam(), fireReportDTO.getIsFire(),
+                fireReportDTO.getFireTeam(), fireReportDTO.getIsActiveFire(),
                 fireReportDTO.getDescription(), fireReportDTO.getFireDepartamentDescription(),
                 fireReportDTO.getArePeople(), fireReportDTO.getIsWood(),
                 fireReportDTO.getIsBuilding(), fireReportDTO.getIsElectricity(),
-                fireReportDTO.getIsHazardousMaterial());
+                fireReportDTO.getIsHazardousMaterial(), fireReportDTO.getPhoto());
 
         UnregisteredUser unregisteredUser = new UnregisteredUser(unregisteredUserDTO.getName(),
                 unregisteredUserDTO.getSurname(), unregisteredUserDTO.getCellPhoneNumber());
